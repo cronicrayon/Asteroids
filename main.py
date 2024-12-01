a548611ef8afd421a4162f68697a08afb4a9209b
croniccrayon <jamiemiles12@gmail.com>
2024-12-01 12:54:50 +0000

    added circleshape.py

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from constants import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop(screen)
+    game_loop()
 
-def game_loop(Screen):
+def game_loop():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0 # Delta time
     while True:
+        dt = timer.tick(60) / 1000
+        # code to get the close window button working
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(Screen, (0,0,0))
-        pygame.display.flip()
-
+                return  
+        pygame.Surface.fill(screen, (0,0,0)) # black background on render window
+        pygame.display.flip() # updates render window
 if __name__ == "__main__":
     main()
