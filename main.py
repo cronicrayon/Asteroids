f90ef9cf27f52e3e6f7b52e597838cb09a79b055
croniccrayon <jamiemiles12@gmail.com>
2024-12-01 13:43:07 +0000

    added player.py and modified main.py to render player on screen

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop()
+    game_loop(screen, timer, player)
 
-def game_loop():
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    timer = pygame.time.Clock()
-    dt = 0 # Delta time
+def game_loop(screen, timer, player):
+    dt = 0 # Delta time - Time since last frame in seconds
     while True:
         dt = timer.tick(60) / 1000
-        # code to get the close window button working
+        # Handle pygame events (like window closing)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
-        pygame.Surface.fill(screen, (0,0,0)) # black background on render window
-        pygame.display.flip() # updates render window
+        pygame.Surface.fill(screen, "black") # Clear the screen before drawing new frame
+        player.draw(screen)
+        pygame.display.flip() # Update the display with all changes
 if __name__ == "__main__":
     main()
