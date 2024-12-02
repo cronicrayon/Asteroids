079621398b98b8e64f5e6db6a513d1a614e9c615
croniccrayon <jamiemiles12@gmail.com>
2024-12-02 14:12:59 +0000

    added a  move method and updated the update method to use move method to move player forward or backwards depending on the key pressed

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 # Player class inherits from Circleshape
 class Player(CircleShape):
@@ -20,8 +20,12 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white",self.triangle(), 2) # magic number is line width
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation) # calulates the direction vector from the player's current rotation
+        self.position += forward * PLAYER_SPEED * dt # Updates player position inline with direction vector
+                                                     # uses delta time for smooth animation
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation += PLAYER_TURN_SPEED * dt # updates self.rotation using delta time for smooth animation
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -32,4 +36,12 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt)
+            pass
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+            pass
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
             pass
